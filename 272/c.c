93493dd4b37950b5d29135da014b211984ec20fb
shintaro0930 <ozaki.shintaro@icloud.com>
2022-10-19 11:00:33 +0900

    not finished but AC is 16/18

--- 272/c.c
+++ 272/c.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 
-// 2要素の和が偶数ならば1, そうでないならば-1をoutput
 
 void qsort(void *base, size_t num, size_t size, int (*compare)(const void *, const void *));
 
 //降順にしたい
-int compare_int(const void *v1, const void *v2)
+int compare_int(const void *a, const void *b)
 {
-    const int _v1 = *((const int *)v1);
-    const int _v2 = *((const int *)v2);
-
-    if (_v1 > _v2)
-    {
-        return 1;
-    }
-    else if (_v1 < _v2)
+    if (*(long *)a > *(long *)b)
     {
         return -1;
+    }
+    else if (*(long *)a < *(long *)b)
+    {
+        return 1;
     }
     else
     {
@@ -29,31 +25,26 @@ int main(void)
     long int n;
     scanf("%ld", &n);
 
-    long long int a[n], max;
+    long int a[n], max;
     for (long int i = 0; i < n; i++)
     {
-        scanf("%lld", &a[i]);
-    }
-    
-    qsort(a, sizeof(a[n]) / sizeof(a[0]), sizeof(long long int), compare_int);
-    max = 0;
+        scanf("%ld", &a[i]);
+    } 
 
-    // debug
-    for (long int i = 0; i < n; i++)
-    {
-        printf("%lld\n", a[i]);
-    }
+    qsort(a, sizeof(a) / sizeof(a[0]), sizeof(long int ), compare_int);
+    max = 0;
 
     for (long int i = 0; i < n; i++)
     {
         for (long int j = i + 1; j < n; j++)
         {
-            // printf("%lldと%lld\n", a[i], a[j]);
             if ((a[i] + a[j]) % 2 == 0)
             {
                 if (max < (a[i] + a[j]))
                 {
                     max = a[i] + a[j];
+                    printf("%ld\n", max);
+                    return 0;
                 }
             }
         }
@@ -61,8 +52,5 @@ int main(void)
 
     if (max == 0)
         printf("-1\n");
-    else
-        printf("%lld\n", max);
-
     return 0;
 }
