af53db65df57d9a1bbbc16469ec2d1e0ad99da8a
shintaro0930 <ozaki.shintaro@icloud.com>
2022-10-19 10:30:32 +0900

    update

--- 272/c.c
+++ 272/c.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
-//2要素の和が偶数ならば1, そうでないならば-1をoutput
+// 2要素の和が偶数ならば1, そうでないならば-1をoutput
 
-void qsort(void *base, size_t num, size_t size, int (*compare)(const void*, const void*));
-
+void qsort(void *base, size_t num, size_t size, int (*compare)(const void *, const void *));
 
 //降順にしたい
 int compare_int(const void *v1, const void *v2)
 {
-  const int _v1 = *((const int*)v1);
-  const int _v2 = *((const int*)v2);
+    const int _v1 = *((const int *)v1);
+    const int _v2 = *((const int *)v2);
 
-  if ( _v1 > _v2 ) {
-    return 1;
-  } else if ( _v1 < _v2 ) {
-    return -1;
-  } else {
-    return 0;
-  }
+    if (_v1 > _v2)
+    {
+        return 1;
+    }
+    else if (_v1 < _v2)
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 int main(void)
@@ -26,32 +30,38 @@ int main(void)
     scanf("%ld", &n);
 
     long long int a[n], max;
-    for(long int i = 0; i < n; i++) {
+    for (long int i = 0; i < n; i++)
+    {
         scanf("%lld", &a[i]);
     }
-
+    
     qsort(a, sizeof(a[n]) / sizeof(a[0]), sizeof(long long int), compare_int);
     max = 0;
 
     // debug
-    for(long int i =0; i < n; i++) {
+    for (long int i = 0; i < n; i++)
+    {
         printf("%lld\n", a[i]);
     }
 
-    for(long int i = 0; i < n; i++) {
-        for(long int j = i + 1; j < n; j++) {
+    for (long int i = 0; i < n; i++)
+    {
+        for (long int j = i + 1; j < n; j++)
+        {
             // printf("%lldと%lld\n", a[i], a[j]);
-            if((a[i] + a[j]) % 2 == 0) {
-                if(max < (a[i] + a[j])) {
+            if ((a[i] + a[j]) % 2 == 0)
+            {
+                if (max < (a[i] + a[j]))
+                {
                     max = a[i] + a[j];
                 }
             }
         }
     }
 
-    if(max == 0) 
+    if (max == 0)
         printf("-1\n");
-    else   
+    else
         printf("%lld\n", max);
 
     return 0;
