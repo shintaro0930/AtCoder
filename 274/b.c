3e782a47e0bc95f0439f8ce6ca90c610674470af
shintaro0930 <ozaki.shintaro@icloud.com>
2022-10-23 13:38:14 +0900

    fixed vommit

--- 274/b.c
+++ 274/b.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
     int a, b;
     scanf("%d %d", &a, &b);
-    int ans[b];    // b
-    char s[b + 1]; // b+1
+    int ans[b];
+    char str[b + 1];
+
+    // initialize
     for (int i = 0; i < b; i++)
-        ans[i] = 0;
+        ans[b] = 0;
+
     for (int i = 0; i < a; i++)
     {
-        scanf("%s", s);
+        scanf("%s", str);
         for (int j = 0; j < b; j++)
         {
-            if (s[j] == '#')
+            if (str[i] == '#')
                 ans[j]++;
         }
     }
+
     for (int i = 0; i < b; i++)
         printf("%d ", ans[i]);
+    printf("\n");
+
     return 0;
 }
